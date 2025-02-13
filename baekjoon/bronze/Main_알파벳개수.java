72e0796bd9cf7b65a342cfe48e412f4c902eafe3
unknown <parkdanil@naver.com>
2025-02-13 17:41:28 +0900

    야미

---
+++ baekjoon/bronze/Main_알파벳개수.java
@@ -0,0 +1,25 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main_알파벳개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[26];
+        char[] c = br.readLine().toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            // int tmp = c[i] - 'a';
+            arr[c[i] - 'a']++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
