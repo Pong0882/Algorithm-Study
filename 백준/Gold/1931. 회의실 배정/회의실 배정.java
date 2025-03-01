56cc4c521f39132af9cb3f928165518905f71880
HelloPong <77256939+Pong0882@users.noreply.github.com>
2025-03-02 00:19:02 +0900

    Update 회의실 배정.java

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -8,6 +8,7 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
