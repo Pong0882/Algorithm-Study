58de0ba3c5ea11af4f4e418fff422e9c69d8536c
Pong <parkdanil@naver.com>
2025-03-11 23:42:27 +0900

    0311

--- baekjoon/gold/Main_학교탐방하기.java
+++ baekjoon/gold/Main_학교탐방하기.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,22 +11,22 @@ public class Main_학교탐방하기 {
     static int N, M;
     static long result;
 
-    static class Edge implements Comparable<Edge> {
-        int e, w;
+    // static class Edge implements Comparable<Edge> {
+    // int e, w;
 
-        public Edge(int e, int w) {
-            this.e = e;
-            this.w = w;
-        }
+    // public Edge(int e, int w) {
+    // this.e = e;
+    // this.w = w;
+    // }
 
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(o.w, w);
-        }
+    // @Override
+    // public int compareTo(Edge o) {
+    // return Integer.compare(o.w, w);
+    // }
 
-    }
+    // }
 
-    static ArrayList<Edge>[] list;
+    static ArrayList<int[]>[] list;
     static boolean visited[];
 
     public static void main(String[] args) throws IOException {
@@ -36,61 +35,75 @@ public class Main_학교탐방하기 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         long max = 0;
-        PriorityQueue<Edge> points = new PriorityQueue<>();
-        st = new StringTokenizer(br.readLine());
-        int aa = Integer.parseInt(st.nextToken());
-        int bb = Integer.parseInt(st.nextToken());
-        int ww = Integer.parseInt(st.nextToken());
+        PriorityQueue<int[]> points = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        PriorityQueue<int[]> Mpoints = new PriorityQueue<>((o1, o2) -> Integer.compare(o2[1], o1[1]));
         list = new ArrayList[N + 1];
         for (int i = 0; i < N + 1; i++) {
             list[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M + 1; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            if (w == 0) {
-                max++;
+            list[s].add(new int[] { e, w });
+            if (s == 0) {
+                continue;
             }
-            list[s].add(new Edge(e, w));
-            list[e].add(new Edge(s, w));
-        }
-        visited = new boolean[N + 1];
-        int cnt = 0;
-        points.add(new Edge(bb, ww));
-        visited[aa] = true;
-        if (ww == 0) {
-            result++;
-            max++;
+            list[e].add(new int[] { s, w });
         }
 
-        while (!points.isEmpty()) {
-            if (cnt == N - 1) {
+        visited = new boolean[N + 1];
+        Mpoints.add(new int[] { 0, list[0].get(0)[0] });
+        int cnt = 0;
+        while (!Mpoints.isEmpty()) {
+            if (cnt == N + 1) {
                 break;
             }
-            Edge tmp = points.poll();
-            if (visited[tmp.e]) {
+            int[] tmp = Mpoints.poll();
+            if (visited[tmp[0]]) {
                 continue;
             }
 
-            visited[tmp.e] = true;
-            if (tmp.w == 0) {
+            visited[tmp[0]] = true;
+            if (tmp[1] == 0) {
+                max++;
+            }
+            cnt++;
+
+            for (int[] edge : list[tmp[0]]) {
+                if (visited[edge[0]]) {
+                    continue;
+                }
+                Mpoints.add(edge);
+            }
+        }
+        visited = new boolean[N + 1];
+        points.add(new int[] { 0, 1 });
+        cnt = 0;
+        while (!points.isEmpty()) {
+            if (cnt == N + 1) {
+                break;
+            }
+            int[] tmp = points.poll();
+            if (visited[tmp[0]]) {
+                continue;
+            }
+            visited[tmp[0]] = true;
+            if (tmp[1] == 0) {
                 result++;
             }
             cnt++;
 
-            for (Edge edge : list[tmp.e]) {
-                if (visited[edge.e]) {
+            for (int[] edge : list[tmp[0]]) {
+                if (visited[edge[0]]) {
                     continue;
                 }
                 points.add(edge);
             }
         }
-        max *= max;
-        result *= result;
-        System.out.println(max - result);
+        System.out.println(result * result - max * max);
 
     }
 }
