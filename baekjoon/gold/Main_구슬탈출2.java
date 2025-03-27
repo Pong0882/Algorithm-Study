0cbd7df1edd9c88148772d99d28553058eb1c723
unknown <parkdanil@naver.com>
2025-03-27 11:23:07 +0900

    일감호 clear

---
+++ baekjoon/gold/Main_구슬탈출2.java
@@ -0,0 +1,73 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_구슬탈출2 {
+    static int R, C;
+    static int[][] paper;
+    static int redr, redc;
+    static int bluer, bluec;
+    static int holer, holec;
+    static int[] dr = { 1, -1, 0, 0 };
+    static int[] dc = { 0, 0, 1, -1 };
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader brr = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(brr.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        paper = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            char[] ch = brr.readLine().toCharArray();
+            for (int j = 0; j < C; j++) {
+                switch (ch[j]) {
+                    case '#':
+                        paper[i][j] = -1;
+                        break;
+                    case 'R':
+                        redr = i;
+                        redc = j;
+                        break;
+                    case 'B':
+                        bluer = i;
+                        bluec = j;
+                        break;
+                    case 'O':
+                        holer = i;
+                        holec = j;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        ArrayDeque<Integer[]> q = new ArrayDeque<>();
+        q.add(new Integer[] { redr, redc, bluer, bluec, 0 });
+
+        while (!q.isEmpty()) {
+            Integer[] tmp = q.poll();
+            int rr = tmp[0];
+            int rc = tmp[1];
+            int br = tmp[2];
+            int bc = tmp[3];
+            int time = tmp[4];
+            for (int i = 0; i < 4; i++) {
+                int nrr = rr + dr[i];
+                int nrc = rc + dc[i];
+                int nbr = br + dr[i];
+                int nbc = bc + dc[i];
+                if (!check(nrr, nrc)) {
+                    continue;
+                }
+            }
+        }
+
+    }
+}
--- baekjoon/gold/Main_일감호.java
+++ baekjoon/gold/Main_일감호.java
@@ -8,10 +8,12 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main_일감호{
-    static long N,M,K;
-    static class Edge implements Comparable<Edge>{
-        int s,w;
+public class Main_일감호 {
+    static int N, M;
+    static long K;
+
+    static class Edge implements Comparable<Edge> {
+        int s, w;
 
         public Edge(int s, int w) {
             this.s = s;
@@ -22,49 +24,55 @@ public class Main_일감호{
         public int compareTo(Edge o) {
             return Integer.compareUnsigned(w, o.w);
         }
-        
 
     }
+
     static boolean[] gongsa;
-    static long[] weight;
+    static int[] weight;
     static PriorityQueue<Edge> pq = new PriorityQueue<>();
     static int[] boss;
     static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-        N = Long.parseLong(st.nextToken());
-        M = Long.parseLong(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         K = Long.parseLong(st.nextToken());
         if (M <= 1) {
             System.out.println("YES");
             return;
         }
-        weight = new long[N+1];
-        gongsa = new boolean[N+1];
+        weight = new int[N + 1];
+        gongsa = new boolean[N + 1];
         st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < N; i++) {
-            weight[i+1] = Integer.parseInt(st.nextToken());
-            pq.add(new Edge(i+1, weight[i+1]));
+            weight[i + 1] = Integer.parseInt(st.nextToken());
+            pq.add(new Edge(i + 1, weight[i + 1]));
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine().trim());
             int tmp = Integer.parseInt(st.nextToken());
-            gongsa[tmp] = true;
+            int tmp2 = Integer.parseInt(st.nextToken());
+            tmp2 = Math.min(tmp, tmp2);
+            if (tmp == N) {
+                tmp2 = tmp;
+            }
+            gongsa[tmp2] = true;
         }
         // System.out.println(Arrays.toString(gongsa));
-        boss = new int[N+1];
-        int rank= 1;
+        boss = new int[N + 1];
+        int rank = 1;
         for (int i = 1; i <= N; i++) {
             boss[i] = rank;
-            if(gongsa[i]){
+            if (gongsa[i]) {
                 rank++;
             }
         }
-        if(rank == boss[N]){
+        if (rank == boss[N]) {
             for (int i = N; i > 0; i--) {
-                if(gongsa[i]){
+                if (gongsa[i]) {
                     break;
                 }
                 boss[i] = boss[1];
@@ -78,17 +86,17 @@ public class Main_일감호{
             Edge cur = pq.poll();
             int building = cur.s;
             int stone = cur.w;
-            if(visited[boss[building]]){
+            if (visited[boss[building]]) {
                 continue;
             }
             visited[boss[building]] = true;
             K -= stone;
-            if(K<0){
+            if (K < 0) {
                 break;
             }
             // System.out.println(K);
             rank--;
-            if(rank == 1){
+            if (rank == 1) {
                 System.out.println("YES");
                 return;
             }
