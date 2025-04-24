3325e1c12304c41e2e29aabd34a050ca4c9b6a41
pong <parkdanil@naver.com>
2025-04-24 21:53:06 +0900

    0424

--- baekjoon/silver/Main_회전초밥.java
+++ baekjoon/silver/Main_회전초밥.java
@@ -3,15 +3,13 @@ package baekjoon.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_회전초밥 {
     static int N, d, K, c;
-    static boolean[] check;
-    static int result;
     static int[] paper;
-    static int resultTmp;
+    static int[] check;
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,56 +20,36 @@ public class Main_회전초밥 {
         K = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        check = new boolean[d + 1];
         paper = new int[N];
+        check = new int[d + 1];
+
         for (int i = 0; i < N; i++) {
             paper[i] = Integer.parseInt(br.readLine());
         }
+        int cnt = 0;
 
-        int left = 0;
-        int right = K-1;
-        for (int i = left; i <= right; i++) {
-            if (paper[i] == c) {
-                right++;
-            }
-            if (check[paper[i]]) {
-                continue;
-            } else {
-                check[paper[i]] = true;
-                resultTmp++;
+        // init
+        for (int i = 0; i < K; i++) {
+            if (check[paper[i]] == 0)
+                cnt++;
+            check[paper[i]]++;
+        }
+
+        result = (check[c] == 0) ? cnt + 1 : cnt;
+
+        for (int i = 1; i < N; i++) {
+            int left = paper[(i - 1) % N];
+            int right = paper[(i + K - 1) % N];
+
+            if (--check[left] == 0)
+                cnt--;
+
+            if (check[right]++ == 0) {
+                cnt++;
+                result = Math.max(result, check[c] == 0 ? cnt + 1 : cnt);
             }
         }
 
-        result = resultTmp;
-        int resultTmp = 0;
-        for (int i = 0; i < N; i++) {
-            left = (left + 1) % N;
-            right = (right + 1) % N;
-
-            if (paper[left - 1] == c) {
-                right++;
-            }
-            if (check[paper[left - 1]]) {
-                check[paper[left - 1]] = false;
-                resultTmp--;
-            } else {
-                check[paper[left - 1]] = true;
-                resultTmp++;
-            }
-
-            if (check[paper[right]]) {
-                continue;
-            } else {
-                check[paper[right]] = true;
-                resultTmp++;
-            }
-
-            result = Math.max(result, resultTmp);
-        }
-        
-        System.out.println();
-        System.out.println(left + " " + right);
-        System.out.println(resultTmp);
-
+        System.out.println(result);
     }
 }
