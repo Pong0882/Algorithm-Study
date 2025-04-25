ed84b589cac84ce59ec8a97938aca80b380ed408
nodb <scnoh02@naver.com>
2025-04-25 09:44:07 +0900

    겹치는건 싫어 111comit

--- baekjoon/silver/Main_겹치는건싫어.java
+++ baekjoon/silver/Main_겹치는건싫어.java
@@ -10,6 +10,7 @@ public class Main_겹치는건싫어 {
     static int[] paper;
     static int result;
     static int[] visited;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,14 +20,27 @@ public class Main_겹치는건싫어 {
 
         st = new StringTokenizer(br.readLine());
         paper = new int[N];
+        int max = 0;
         for (int i = 0; i < N; i++) {
             paper[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, paper[i]);
         }
         int left = 0;
         int right = 0;
-        visited = new int[N];
-        while (true) {
-
+        visited = new int[max+1];
+        while (right < N && left < N) {
+            // System.out.println("left : " + left + ", right : " + right);
+            // System.out.println("visited[paper[right]] : " + visited[paper[right]] + " ,
+            // paper[right] : "+ paper[right]);
+            if (visited[paper[right]] >= K) {
+                visited[paper[left++]]--;
+                cnt--;
+            } else {
+                visited[paper[right++]]++;
+                cnt++;
+            }
+            result = Math.max(cnt, result);
         }
+        System.out.println(result);
     }
 }
