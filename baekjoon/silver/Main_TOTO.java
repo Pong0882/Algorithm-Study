cd06aa06241db40c15bcf6f3af3558eb4ee840b2
Pong <parkdanil@naver.com>
2025-05-06 13:35:24 +0900

    wait

---
+++ baekjoon/silver/Main_TOTO.java
@@ -0,0 +1,9 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+
+public class Main_TOTO {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader()
+    }
+}
